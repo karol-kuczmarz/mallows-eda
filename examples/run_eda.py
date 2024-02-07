5cfb039888c1edcc2fe3d9b716c787e740bc55ec
Karol Kuczmarz <kj.kuczmarz@gmail.com>
2024-02-08 00:19:13 +0100

    Add faster sampling

--- examples/run_eda.py
+++ examples/run_eda.py
@@ -1,14 +1,15 @@
 from mallows import config
 from mallows.distribution import Mallows
 from mallows.eda import EDA
-from mallows.selection import top_k_selection
+from mallows.selection import get_linear_ranking_selection, top_k_selection
 from mallows.tsp_utils import get_tsp_problem, plot_solution
 
-problem_size = 29
-population_size = (problem_size - 1) * 100
-selection_size = (problem_size - 1) * 50
-offspring_size = (problem_size - 1) * 100 - 1
+problem_size = 16
+population_size = (problem_size - 1) * 10000
+selection_size = (problem_size - 1) * 5000
+offspring_size = (problem_size - 1) * 10000 - 1
 n_iter = 100 * (problem_size - 1)
+selection_function = get_linear_ranking_selection(0.05, 1.95)
 
 coords, dist_matrix, objective_function, optimal_solution = get_tsp_problem(
     config.DATA_DIR / "tsp", "bayg29"
--- src/mallows/selection.py
+++ src/mallows/selection.py
@@ -1,2 +1,18 @@
+import numpy as np
+
+
 def top_k_selection(population_objectives, k):
     return population_objectives.argsort()[:k]
+
+
+def get_linear_ranking_selection(alpha, beta):
+    assert 0 <= alpha and alpha <= beta and beta <= 2 and alpha + beta <= 2
+
+    def linear_ranking_selection(population_objectives, k):
+        N = population_objectives.shape[0]
+        ranks = np.argsort(population_objectives)[::-1]
+        probabilities = (alpha + (ranks / (N - 1)) * (beta - alpha)) / N
+        selected_indices = np.random.choice(N, k, replace=True, p=probabilities)
+        return selected_indices
+
+    return linear_ranking_selection
